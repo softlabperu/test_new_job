1b6eeed5c24596cf82e5cd182320344e0bb26e3f
herbert kevin acosta navarro <kacosta@softlabperu.com>
2019-04-26 14:59:15 -0500

    agregar modelos en el django admin

--- apps/cuestionario/admin.py
+++ apps/cuestionario/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.cuestionario.models import Categoria, Nivel, Pregunta, Opcion, Participante, ExamenPregunta, Puntaje, Examen
+
+admin.site.register(Categoria)
+admin.site.register(Nivel)
+
+admin.site.register(Pregunta)
+admin.site.register(Opcion)
+
+admin.site.register(Participante)
+admin.site.register(Examen)
+
+admin.site.register(ExamenPregunta)
+admin.site.register(Puntaje)
